f0bf942bb2a3b31d433c28d9870b23a3613b10fc
Lucas Rezende <arrobagemeioponto@gmail.com>
2021-03-22 18:15:29 -0300

    feat: make components dynamic

--- src/components/Pulse/index.tsx
+++ src/components/Pulse/index.tsx
@@ -1,38 +1,60 @@
+//----------------------------------------------------------------< functions >
+import { memo } from "react";
 //--------------------------------------------------------------------< types >
+import { MouseEvent } from "react";
 interface PulseProps {
-  x: string;
-  y: string;
+  x: string | number;
+  y: string | number;
   gap: number;
   amount: number;
   color: string;
 }
 //================================================================[ < Pulse > ]
-export function Pulse({ x, y, amount, gap, color }: PulseProps) {
+function Pulse({ x, y, amount, gap, color }: PulseProps) {
   //----------------------------------------------------------------< methods >
+  function handleMouseEnter(e: MouseEvent<SVGCircleElement>) {
+    e.currentTarget.style.opacity = "1";
+  }
+  function handleMouseOut(e: MouseEvent<SVGCircleElement>) {
+    e.currentTarget.style.opacity = "0";
+  }
+  //---------------------------------------------------------------------------
   function drawPulses() {
     const pulses = [];
 
     for (let i = 1; i <= amount; i++) {
       pulses.push(
-        <circle
-          key={i}
-          cx={x}
-          cy={y}
-          r={gap * i + "rem"}
-          strokeWidth="1"
-          stroke={`var(--${color})`}
-          fill="none"
-        />
+        <g key={i}>
+          <circle
+            className="display"
+            cx={x}
+            cy={y}
+            r={gap * i}
+            stroke={`var(--${color})`}
+          />
+          <circle
+            className="hover"
+            cx={x}
+            cy={y}
+            r={gap * i}
+            stroke={`var(--${color})`}
+            onMouseEnter={handleMouseEnter}
+            onMouseOut={handleMouseOut}
+          />
+        </g>
       );
     }
 
     return pulses;
   }
   //-----------------------------------------------------------------< return >
+  console.log("pulse rendered");
   return (
-    <>
-      <circle cx={x} cy={y} r="0.25rem" fill={`var(--${color})`} />
+    <g className="pulse-container">
+      <circle className="center" cx={x} cy={y} fill={`var(--${color})`} />
       {drawPulses()}
-    </>
+    </g>
   );
 }
+
+export default memo(Pulse);
--- src/components/Window/index.tsx
+++ src/components/Window/index.tsx
@@ -1,13 +1,66 @@
 //---------------------------------------------------------------< components >
-import { Pulse } from "../Pulse";
+import Pulse from "../Pulse";
+import { PulseCreator } from "../PulseCreator";
+//--------------------------------------------------------------------< hooks >
+import { useContext, useEffect, useState } from "react";
+//-----------------------------------------------------------------< contexts >
+import { PulsesContext } from "../../contexts/PulsesContext";
 //-------------------------------------------------------------------< styles >
 import "./styles.css";
+//--------------------------------------------------------------------< types >
+import { MouseEvent } from "react";
 //===============================================================[ < Window > ]
 export function Window() {
+  //-------------------------------------------------------------< properties >
+  const { pulses, addPulse } = useContext(PulsesContext);
+  //---------------------------------------------------------------------------
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+
+  const [isPulseCreating, setIsPulseCreating] = useState(false);
+  //----------------------------------------------------------------< methods >
+  useEffect(() => {
+    addPulse({
+      x: window.innerWidth / 2,
+      y: window.innerHeight / 2,
+      amount: 3,
+      gap: 36,
+      color: "foreground",
+    });
+  }, []);
+
+  useEffect(() => {
+    setIsPulseCreating(false);
+  }, [pulses]);
+  //---------------------------------------------------------------------------
+  function handleClick(e: MouseEvent<SVGSVGElement>) {
+    setX(e.clientX);
+    setY(e.clientY);
+
+    setIsPulseCreating(true);
+  }
   //-----------------------------------------------------------------< return >
+  console.log("window rendered");
   return (
-    <svg className="window-container">
-      <Pulse x="50%" y="50%" gap={2} amount={3} color="foreground" />
-    </svg>
+    <>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="window-container"
+        onClick={handleClick}
+      >
+        {pulses.map(({ x, y, gap, amount, color }, index) => (
+          <Pulse
+            key={index}
+            x={x}
+            y={y}
+            gap={gap}
+            amount={amount}
+            color={color}
+          />
+        ))}
+      </svg>
+
+      {isPulseCreating && <PulseCreator x={x} y={y} color="orange" />}
+    </>
   );
 }
