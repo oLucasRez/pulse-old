392e032acb2480e00440c66ac3dbda616f16413c
Lucas Rezende <arrobagemeioponto@gmail.com>
2021-03-22 23:12:34 -0300

    refactor(window): fix responsability
    'window' component deal with the layers of the application

--- src/components/Window/index.tsx
+++ src/components/Window/index.tsx
@@ -1,66 +1,14 @@
 //---------------------------------------------------------------< components >
-import Pulse from "../Pulse";
-import { PulseCreator } from "../PulseCreator";
-//--------------------------------------------------------------------< hooks >
-import { useContext, useEffect, useState } from "react";
-//-----------------------------------------------------------------< contexts >
-import { PulsesContext } from "../../contexts/PulsesContext";
-//-------------------------------------------------------------------< styles >
-import "./styles.css";
-//--------------------------------------------------------------------< types >
-import { MouseEvent } from "react";
+import { PulsesDisplayerLayer } from "../PulsesDisplayerLayer";
+import { PulseCreatorLayer } from "../PulseCreatorLayer";
 //===============================================================[ < Window > ]
 export function Window() {
-  //-------------------------------------------------------------< properties >
-  const { pulses, addPulse } = useContext(PulsesContext);
-  //---------------------------------------------------------------------------
-  const [x, setX] = useState(0);
-  const [y, setY] = useState(0);
-
-  const [isPulseCreating, setIsPulseCreating] = useState(false);
-  //----------------------------------------------------------------< methods >
-  useEffect(() => {
-    addPulse({
-      x: window.innerWidth / 2,
-      y: window.innerHeight / 2,
-      amount: 3,
-      gap: 36,
-      color: "foreground",
-    });
-  }, []);
-
-  useEffect(() => {
-    setIsPulseCreating(false);
-  }, [pulses]);
-  //---------------------------------------------------------------------------
-  function handleClick(e: MouseEvent<SVGSVGElement>) {
-    setX(e.clientX);
-    setY(e.clientY);
-
-    setIsPulseCreating(true);
-  }
   //-----------------------------------------------------------------< return >
   console.log("window rendered");
   return (
     <>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        className="window-container"
-        onClick={handleClick}
-      >
-        {pulses.map(({ x, y, gap, amount, color }, index) => (
-          <Pulse
-            key={index}
-            x={x}
-            y={y}
-            gap={gap}
-            amount={amount}
-            color={color}
-          />
-        ))}
-      </svg>
-
-      {isPulseCreating && <PulseCreator x={x} y={y} color="orange" />}
+      <PulsesDisplayerLayer />
+      <PulseCreatorLayer color="orange" />
     </>
   );
 }
