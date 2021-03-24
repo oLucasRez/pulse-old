68a527ffb730733bc2c70661af4bbc65c41f0b7e
Lucas Rezende <arrobagemeioponto@gmail.com>
2021-03-24 19:15:03 -0300

    feat(pulses-context): add 'clear pulses' function

--- src/contexts/PulsesContext.tsx
+++ src/contexts/PulsesContext.tsx
@@ -5,27 +5,32 @@ import { createContext } from "react";
 //--------------------------------------------------------------------< types >
 import { IPulse } from "../types/IPulse";
 import { ReactNode } from "react";
-interface PulsesContextData {
+interface IData {
   pulses: IPulse[];
   addPulse: (pulse: IPulse) => void;
+  clearPulses: () => void;
 }
-interface PulsesProviderProps {
+interface IProps {
   children: ReactNode;
 }
 //-------------------------------------------------------------------< global >
-export const PulsesContext = createContext({} as PulsesContextData);
+export const PulsesContext = createContext({} as IData);
 //=======================================================[ < PulsesProvider > ]
-export function PulsesProvider({ children }: PulsesProviderProps) {
+export function PulsesProvider({ children }: IProps) {
   //-------------------------------------------------------------< properties >
   const [pulses, setPulses] = useState<IPulse[]>([]);
   //----------------------------------------------------------------< methods >
   function addPulse(pulse: IPulse) {
     setPulses([...pulses, pulse]);
   }
+
+  function clearPulses() {
+    setPulses([]);
+  }
   //-----------------------------------------------------------------< return >
   console.log("pulses-provider rendered");
   return (
-    <PulsesContext.Provider value={{ pulses, addPulse }}>
+    <PulsesContext.Provider value={{ pulses, addPulse, clearPulses }}>
       {children}
     </PulsesContext.Provider>
   );
